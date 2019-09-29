aac06919dc174e7b91f4122a341b99e3133403b1
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-29 15:11:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,9 +8,12 @@ class School
   end
   
   def add_student(student_name, grade)
-    binding.pry
-    roster[grade] = []
-    roster[grade] << student_name
+    if roster[grade]
+      roster[grade] << student_name
+    else
+      roster[grade] = []
+      roster[grade] << student_name
+    end
   end
   
 #  def roster
